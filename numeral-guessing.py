c4e9ae10842cf8a9c8aa8c5a055d1647f8649af1
FelipeM0reira <felipeemooreira@gmail.com>
2022-05-02 14:12:23 -0300

    first commit

--- numeral-guessing.py
+++ numeral-guessing.py
@@ -5,5 +5,5 @@ attempts_list = []
 def show_score():
     if len(attempts_list) <= 0:
         print("There is currently no high score, it's yours for the taking!")
-        else:
-            print("The current high score is {} attempts".format(min(attempts_list)))
+    else:
+        print("The current high score is {} attempts".format(min(attempts_list)))
